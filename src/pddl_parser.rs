c70f4c03ff5864bfba5c4a872ea2ce32a300f9fc
Ivy <ivyjs@hendrix.edu>
2020-06-23 12:03:25 -0500

    Added SatelliteAsString, decode, obj_get, removed commented out code

--- src/pddl_parser.rs
+++ src/pddl_parser.rs
@@ -112,13 +112,13 @@ SATELLITE STUFF
 //     }
 // }
 
-pub fn make_satellite_problem_from(pddl_file: &str) -> io::Result<(SatelliteState,SatelliteGoals)>{
-    let contents = fs::read_to_string(pddl_file)?.to_lowercase();
-    match Define::parse(contents.as_str()) {
-        Ok(parsed) => Ok(parsed.init_and_goal()),
-        Err(e) => {println!("{}", e); Err(err!(Other, "oops"))}
-    }
-}
+// pub fn make_satellite_problem_from(pddl_file: &str) -> io::Result<(SatelliteState,SatelliteGoals)>{
+//     let contents = fs::read_to_string(pddl_file)?.to_lowercase();
+//     match Define::parse(contents.as_str()) {
+//         Ok(parsed) => Ok(parsed.init_and_goal()),
+//         Err(e) => {println!("{}", e); Err(err!(Other, "oops"))}
+//     }
+// }
 
 #[derive(Sexpy)]
 struct Define {
@@ -134,53 +134,64 @@ fn decode_on(p: &Predicate, objects: &HashMap<String,usize>) -> (usize, usize) {
     let bottom = obj_get(p, objects, 1);
     (top, bottom)
 }
+fn obj_get(p: &Predicate, objects: &HashMap<String,usize>, i: usize) -> usize {
 
-impl Define{
-    pub fn init_and_goal(&self)->(SatelliteState,SatelliteGoals){
-        let mut objects = HashMap::new();
-        for object in self.objects.objs.iter() {
-            objects.insert(String::from(object), objects.len());
-        }
-
-        //The predicates we have are as follows:
-        let mut onboard: BTreeMap<SatelliteEnum, Vec<SatelliteEnum>>= BTreeMap::new();
-        let mut supports: BTreeMap<SatelliteEnum, SatelliteEnum> =BTreeMap::new();
-        let mut pointing: BTreeMap<SatelliteEnum, SatelliteEnum> =BTreeMap::new();
-        let mut power_avail = false;
-        let mut power_on: Vec<SatelliteEnum> = vec![];
-        let mut calibrated: Vec<SatelliteEnum> = vec![];
-        let mut have_image: BTreeMap<SatelliteEnum, SatelliteEnum> = BTreeMap::new();
-        let mut calibration_target: BTreeMap<SatelliteEnum, SatelliteEnum> =  BTreeMap::new();
-
-        let mut goals_images: BTreeMap<SatelliteEnum, SatelliteEnum> = BTreeMap::new();
-        let mut goal_fuel_used: u32 = 0;
-
-
-
-        for pred in self.init.predicates.iter(){
-            if pred.predicate_type == "onboard".parse().unwrap() {
-                onboard.insert(SatelliteEnum::from(decode_on(pred, &objects)), pred.len());
-            }else if pred.predicate_type == "supports".parse().unwrap() {
-                supports.insert(SatelliteEnum::from(pred), pred.len());
-            }else if pred.predicate_type== "pointing".parse().unwrap() {
-                pointing.insert(SatelliteEnum::from(pred), pred.len());
-            }else if pred.predicate_type == "power_avail".parse().unwrap() {
-                power_avail = true;
-            } else if pred.predicate_type == "power_on".parse().unwrap() {
-                power_on.push(SatelliteEnum::from(pred));
-            }else if pred.predicate_type == "calibrated".parse().unwrap() {
-                calibrated.push(SatelliteEnum::from(pred));
-            }else if pred.predicate_type == "have_image".parse().unwrap() {
-                have_image.insert(SatelliteEnum::from(pred), pred.len());
-            }else if pred.predicate_type == "calibration_target".parse().unwrap() {
-                calibration_target.insert(SatelliteEnum::from(pred), pred.len());
-            }
-        }
-
-
-        (SatelliteState::new(onboard,supports,pointing,power_avail,power_on,calibrated,have_image,calibration_target), SatelliteGoals::new(goals_images,  goal_fuel_used));
-    }
+    *objects.get(p.predicate_args[i].as_str()).unwrap()
 }
+
+// struct
+
+
+// impl Define{
+//     pub fn init_and_goal(&self)->(SatelliteState,SatelliteGoals){
+//
+//
+//
+//         let mut objects = HashMap::new();
+//         for object in self.objects.objs.iter() {
+//             objects.insert(String::from(object), objects.len());
+//         }
+//
+//         //The predicates we have are as follows:
+//         let mut onboard: BTreeMap<SatelliteEnum, Vec<SatelliteEnum>>= BTreeMap::new();
+//         let mut supports: BTreeMap<SatelliteEnum, SatelliteEnum> =BTreeMap::new();
+//         let mut pointing: BTreeMap<SatelliteEnum, SatelliteEnum> =BTreeMap::new();
+//         let mut power_avail = false;
+//         let mut power_on: Vec<SatelliteEnum> = vec![];
+//         let mut calibrated: Vec<SatelliteEnum> = vec![];
+//         let mut have_image: BTreeMap<SatelliteEnum, SatelliteEnum> = BTreeMap::new();
+//         let mut calibration_target: BTreeMap<SatelliteEnum, SatelliteEnum> =  BTreeMap::new();
+//
+//         let mut goals_images: BTreeMap<SatelliteEnum, SatelliteEnum> = BTreeMap::new();
+//         let mut goal_fuel_used: u32 = 0;
+//
+//
+//
+//
+//         for pred in self.init.predicates.iter(){
+//             if pred.predicate_type == "onboard".parse().unwrap() {
+//                 onboard.insert(SatelliteEnum::from(decode_on(pred, &objects)), pred.len());
+//             }else if pred.predicate_type == "supports".parse().unwrap() {
+//                 supports.insert(SatelliteEnum::from(pred), pred.len());
+//             }else if pred.predicate_type== "pointing".parse().unwrap() {
+//                 pointing.insert(SatelliteEnum::from(pred), pred.len());
+//             }else if pred.predicate_type == "power_avail".parse().unwrap() {
+//                 power_avail = true;
+//             } else if pred.predicate_type == "power_on".parse().unwrap() {
+//                 power_on.push(SatelliteEnum::from(pred));
+//             }else if pred.predicate_type == "calibrated".parse().unwrap() {
+//                 calibrated.push(SatelliteEnum::from(pred));
+//             }else if pred.predicate_type == "have_image".parse().unwrap() {
+//                 have_image.insert(SatelliteEnum::from(pred), pred.len());
+//             }else if pred.predicate_type == "calibration_target".parse().unwrap() {
+//                 calibration_target.insert(SatelliteEnum::from(pred), pred.len());
+//             }
+//         }
+//
+//
+//         (SatelliteState::new(onboard,supports,pointing,power_avail,power_on,calibrated,have_image,calibration_target), SatelliteGoals::new(goals_images,  goal_fuel_used));
+//     }
+// }
 
 #[derive(Sexpy)]
 struct Problem {
@@ -207,14 +218,14 @@ struct Init {
 
 #[derive(Sexpy)]
 #[sexpy(nohead)]
-struct Predicate {
+pub struct Predicate {
     predicate_type: String,
     predicate_args: Vec<String>,
     nested_predicate: Vec<NestedPredicate>
 }
 
 #[derive(Sexpy)]
-#[sexpy(heaed ="=")]
+#[sexpy(head ="=")]
 struct NestedPredicate {
     predicate_type: String,
     predicate_args: Vec<String>
@@ -229,4 +240,45 @@ struct Goal {
 #[derive(Sexpy)]
 struct And {
     goals: Vec<Predicate>
+}
+
+pub struct SatelliteAsString{
+    pub onboard: BTreeMap<String, u32>,
+    pub supports: BTreeMap<String, u32>,
+    pub pointing: BTreeMap<String, u32>,
+    pub power_on:BTreeMap<String, u32>,
+    pub calibrated: BTreeMap<String, u32>,
+    pub have_image: BTreeMap<String, u32>,
+    pub calibration_target: BTreeMap<String, u32>,
+}
+impl SatelliteAsString{
+    /*
+    fn decode_on(p: &Predicate, objects: &HashMap<String,usize>) -> (usize, usize) {
+    let top = obj_get(p, objects, 0);
+    let bottom = obj_get(p, objects, 1);
+    (top, bottom)
+}
+fn obj_get(p: &Predicate, objects: &HashMap<String,usize>, i: usize) -> usize {
+
+    *objects.get(p.predicate_args[i].as_str()).unwrap()
+}
+     */
+    pub fn decode(self,p: &Predicate, objects: &HashMap<String, usize>, name: String) -> u32{
+        match self.obj_get(p,objects, name) {
+            Some(n) => n,
+            None => 0
+            }
+        }
+    pub fn obj_get(self, p: &Predicate, objects: &HashMap<String, usize>, name: String) -> Option<u32> {
+        match name.as_str(){
+            "onboard" => Some(*self.onboard.get(p.predicate_args[0].as_str())?),
+            "supports" => Some(*self.supports.get(p.predicate_args[0].as_str())?),
+            "pointing" => Some(*self.pointing.get(p.predicate_args[0].as_str())?),
+            "power_on" => Some(*self.supports.get(p.predicate_args[0].as_str())?),
+            "calibrated" => Some(*self.supports.get(p.predicate_args[0].as_str())?),
+            "have_image" => Some(*self.supports.get(p.predicate_args[0].as_str())?),
+            "calibration_target" => Some(*self.supports.get(p.predicate_args[0].as_str())?),
+            _ => None
+        }
+    }
 }
