531fccc8918de8246bf3968260aa0b4639fa9b07
Ivy <ivyjs@hendrix.edu>
2020-06-29 10:16:55 -0500

    Completed extract_state

--- src/pddl_parser.rs
+++ src/pddl_parser.rs
@@ -95,15 +95,25 @@ fn extract_state(parsed: &PddlProblem, objects: &BTreeMap<String,u32>) -> Satell
         if pred.get_tag() == "on_board" {
             let onboard_inserter = decode_onboard(&pred, &objects);
             let turn_into_instrument = |n| Instrument(n); //This needs to be mapped onto onboard_inserted.1
-            onboard.insert(Satellite(onboard_inserter.0), (onboard_inserter.1.iter().map(turn_into_instrument).collect()));
+            onboard.insert(Satellite(onboard_inserter.0), (onboard_inserter.1.into_iter().map(turn_into_instrument).collect()));
         } else if pred.get_tag() == "supports" {
             let supports_inserter = decode_supports(&pred, &objects);
             let turn_into_mode = |n| Mode(n);
-            supports.insert(Instrument(supports_inserter.0), (supports_inserter.1.iter().map(turn_into_mode).collect()));
+            supports.insert(Instrument(supports_inserter.0), (supports_inserter.1.into_iter().map(turn_into_mode).collect()));
+        }else if pred.get_tag() == "direction" {
+            let decoded_pointing = decode_calibration_target(&pred, &objects);
+            pointing.insert(Satellite(decoded_pointing.0), Direction(decoded_pointing.1));
+        }else if pred.get_tag() == "power_avail" {
+            power_avail = true;
+        } else if pred.get_tag() == "calibrated" {
+            calibrated.push(Instrument(*objects.get(pred.get_arg(0)).unwrap()));
+        }else if pred.get_tag() == "have_image" {
+            let decoded_have_image = decode_calibration_target(&pred, &objects);
+            have_image.insert(Direction(decoded_have_image.0), Mode(decoded_have_image.1));
+        }else if pred.get_tag() == "calibration_target" {
+            let decoded_calibration_target = decode_calibration_target(&pred, &objects);
+            calibration_target.insert(Instrument(decoded_calibration_target.0), Direction(decoded_calibration_target.1));
         }
-        // }else if pred.get_tag() == "direction" {
-        //     pointing.push(decode_pointing(&pred, &objects));
-        // }
     }
     return SatelliteState::new(onboard,supports,pointing,power_avail,power_on,calibrated,have_image,calibration_target);
 }
@@ -136,6 +146,18 @@ fn decode_pointing(p: &Predicate, objects: &BTreeMap<String,u32>) -> (u32, u32) 
     (satellite, direction)
 }
 
+fn decode_have_image(p: &Predicate, objects: &BTreeMap<String,u32>) -> (u32, u32) {
+    let direction = obj_get(p.get_arg(0), objects);
+    let mode = obj_get(p.get_arg(1), objects);
+    (direction, mode)
+}
+
+fn decode_calibration_target(p: &Predicate, objects: &BTreeMap<String,u32>) -> (u32, u32) {
+    let instrument = obj_get(p.get_arg(0), objects);
+    let direction = obj_get(p.get_arg(1), objects);
+    (instrument, direction)
+}
+
 fn obj_get(obj_name: &str, objects: &BTreeMap<String,u32>) -> u32 {
     *(objects.get(obj_name).unwrap())
 }
@@ -144,7 +166,7 @@ fn obj_get(obj_name: &str, objects: &BTreeMap<String,u32>) -> u32 {
 fn extract_goals(parsed: &PddlProblem, objects: &BTreeMap<String,u32>) -> Vec<(u32,u32)> {
     let mut goals = Vec::new();
     for goal in parsed.goals.iter() {
-        goals.push(decode_pointing(&goal, &objects));
+        goals.push(decode_calibration_target(&goal, &objects));
     }
     goals
 }
